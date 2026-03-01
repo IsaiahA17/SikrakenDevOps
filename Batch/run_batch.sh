0fee0ad8cc1e09321fed9c92495f3c1a74d2fbe9
IsaiahA17 <nashandres5665@gmail.com>
2026-03-01 16:04:08 +0000

    Adding Stack Size Argument

--- Batch/run_batch.sh
+++ Batch/run_batch.sh
@@ -7,6 +7,7 @@ TASK_COUNT="${3:-${TASK_COUNT:-5}}"
 CATEGORY="${4:-${CATEGORY:-chris}}"
 BUDGET="${5:-${BUDGET:-10}}"
 MODE="${6:-${MODE:-release}}"
+STACK_SIZE_GB="${7:-${STACK_SIZE_GB:-3072}}"
 TIMESTAMP=$(date -u +"%Y_%m_%d_%H_%M")
 
 JOB_ID=$(aws batch submit-job \
@@ -18,7 +19,8 @@ JOB_ID=$(aws batch submit-job \
     {name=CATEGORY,value=$CATEGORY},
     {name=BUDGET,value=$BUDGET},
     {name=MODE,value=$MODE},
-    {name=TIMESTAMP,value=$TIMESTAMP}
+    {name=TIMESTAMP,value=$TIMESTAMP},
+    {name=STACK_SIZE_GB,value=$STACK_SIZE_GB}
   ]" \
   --query 'jobId' \
   --output text
