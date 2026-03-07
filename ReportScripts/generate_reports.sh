687a5c02212eaf7056d91d6edd4507f1fcbcf3ec
IsaiahA17 <nashandres5665@gmail.com>
2026-03-07 20:20:57 +0000

    Fixed Errors Variables And Arguments

--- ReportScripts/generate_reports.sh
+++ ReportScripts/generate_reports.sh
@@ -1,4 +1,6 @@
-s3_bucket="${2:-${S3_BUCKET_NAME:-ecs-benchmarks-output}}"
+#!/bin/bash
+
+s3_bucket="${1:-${S3_BUCKET_NAME:-ecs-benchmarks-output}}"
 CATEGORY="${2:-${CATEGORY:-ECA}}"
 
 aws s3 cp s3://$s3_bucket/$CATEGORY/ category_results/ --recursive
@@ -9,6 +11,8 @@ touch $TIMESTAMP_DIR/benchmark_files.txt
 cat $TIMESTAMP_DIR/benchmark_files/* > $TIMESTAMP_DIR/benchmark_files.txt
 
 echo "Timestamp dir: $TIMESTAMP_DIR"
-SikrakenDevOps/ReportScripts/create_category_test_run_table.sh "$TIMESTAMP_DIR"
-python ./SikrakenPythonScripts/filepath_to_url_processor.py "$TIMESTAMP_DIR" --run_folder "$TIMESTAMP_NAME" --s3_bucket "$s3_bucket" --category "$CATEGORY"
+/app/ReportScripts/create_category_test_run_table.sh "$TIMESTAMP_DIR"
+
+TIMESTAMP_NAME=$(basename "$TIMESTAMP_DIR")
+python3 /app/SikrakenPythonScripts/filepath_to_url_processor.py "$TIMESTAMP_DIR" --run_folder "$TIMESTAMP_NAME" --s3_bucket "$s3_bucket" --category "$CATEGORY"
 aws s3 cp "$TIMESTAMP_DIR/category_test_run_results.html" "s3://$s3_bucket/$CATEGORY/$TIMESTAMP_NAME/category_test_run_results.html" --content-type text/html
